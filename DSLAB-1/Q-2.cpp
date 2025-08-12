1e5a965a1b94607be687e484c8de556c0b9b6d6b
Vidhita-Mehta <vmehta1_be24@thapar.edu>
2025-08-12 17:32:46 +0530

    Add files via upload

--- DSLAB-1/Q-2.cpp
+++ DSLAB-1/Q-2.cpp
@@ -1,32 +1,40 @@
-//Removing the duplicate elements in an array
 #include<iostream>
 using namespace std;
+
 int main()
 {
-	int arr[10];
-	int n=10;
-	cout<<"ENter the array elements";
-	for(int i=0;i<10;i++)
-	{
-		cin>>arr[i];
-	}
-	for(int i=0;i<n;i++)
-	{
-		for(int j=i+1;j<n;j++)
-		{
-			
-			if(arr[i]==arr[j])
-			{	cout<<"works";
-			for(int k=j;k<n-1;k++)
-			{
-				arr[k]=arr[k+1];
-			}
-			n--;
-			}
-	}
-		}
-	cout<<"Array after removing duplicates"<<endl;
-	for(int i=0;i<n;i++)
-	cout<<arr[i]<<endl;
+    int arr[10];
+    int n = 10;
+
+    cout << "Enter the array elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; )
+        {
+            if (arr[i] == arr[j])
+            {
+                for (int k = j; k < n - 1; k++)
+                {
+                    arr[k] = arr[k + 1];
+                }
+                n--; 
+            }
+            else
+            {
+                j++; 
+				            }
+        }
+    }
+
+    cout << "Array after removing duplicates" << endl;
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
 }
 
